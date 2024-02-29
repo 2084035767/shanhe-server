279afbd9cbf083bdd46e4ee0d8ec4504047c1e64
zs <2084035767@qq.com>
2024-02-29 20:59:11 +0800

    frist commit

---
+++ src/main/java/com/zs/shanhe/ShanheApplication.java
@@ -0,0 +1,12 @@
+package com.zs.shanhe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShanheApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(ShanheApplication.class, args);
+  }
+}
